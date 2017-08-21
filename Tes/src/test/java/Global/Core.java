24cb85add110c3fcf687aae8bab2a4bb5da29919
Uy Nguyen <uy.nguyen@tidal.com>
2017-08-21 17:35:52 +0200

    Adds something to core

--- Tes/src/test/java/Global/Core.java
+++ Tes/src/test/java/Global/Core.java
@@ -3,6 +3,7 @@ package Global;
 import org.testng.annotations.BeforeMethod;
 
 public class Core {
+    // Will always run first for tests that requires user to be signed in.
     @BeforeMethod(alwaysRun = true)
     public void basicWebclientSetup() {
         System.out.println("this runs before the others yes yes");
