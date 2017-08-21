f2fd84e302c6516eaa7e36d0683d65cd04fdc458
Uy Nguyen <uy.nguyen@tidal.com>
2017-08-21 18:39:37 +0200

    Implemented login beforemethod for all the future tests that will require it.

--- Tes/src/test/java/Global/Core.java
+++ Tes/src/test/java/Global/Core.java
@@ -1,29 +1,55 @@
 package Global;
 
+import Website.WebsiteConstants;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
+
 
 public class Core {
     // Will always run first for tests that requires user to be signed in.
+    // Future goal is also to make driver global, so that we can grab it from anywhere ;)
     @BeforeMethod(alwaysRun = true)
     public void basicWebclientSetup() {
         //Set environment blabla, to be featured later
-        setupLogin();
+        setupLogin("uytestautomation@gmail.com","testautomation");
     }
 
-    public void setupLogin() {
-        System.setProperty("webdriver.chrome.driver", "/Users/tidal");
+    // Will go to youtube.com, click log in manually, and proceed to log in.
+    public void setupLogin(String name, String pass) {
             // Optional, if not specified, WebDriver will search your path for chromedriver.
-         WebDriver driver = new ChromeDriver();
-            driver.get("http://www.google.com/xhtml");
-            WebElement searchBox = driver.findElement(By.name("q"));
-            searchBox.sendKeys("ChromeDriver");
-            searchBox.submit();
-            driver.quit();
+            WebDriver driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.get("https://www.youtube.com");
+
+            // Currently using xPath because youtube has no consistent jss css that we can use for cssSelector.
+            WebElement elementToClick = driver.findElement(By.xpath("//*[contains(text(), 'Sign in')]"));
+            elementToClick.click();
+
+            // Input the email
+            WebElement inputFormEmail = driver.findElement(By.cssSelector("input[type='email']"));
+            inputFormEmail.sendKeys(name);
+            inputFormEmail.sendKeys(Keys.RETURN);
+
+            // Input the password, will implement webdriverwait in the future.
+
+        try {
+            sleep(1000);
+        } catch(InterruptedException e) {
+            System.out.println("gon wait");
         }
+            WebElement inputFormPassword = driver.findElement(By.cssSelector("input[type='password']"));
+            inputFormPassword.sendKeys(pass);
+            inputFormPassword.sendKeys(Keys.RETURN);
+
+
+    }
 
     }
 
---
+++ Tes/src/test/java/Website/WebsiteConstants.java
@@ -0,0 +1,7 @@
+package Website;
+
+public class WebsiteConstants {
+    public static final long IMPLICIT_WAIT_TIME = 15;
+    public static final long PAGE_LOAD_TIMEOUT = 30;
+
+}
