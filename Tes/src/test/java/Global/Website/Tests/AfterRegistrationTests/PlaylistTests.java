887a6b284b71ad4a410d55eb17c679e3cbb4e6c0
Uy Nguyen <uy.nguyen@tidal.com>
2017-08-21 17:01:50 +0200

    Maven, testNG, JUnit added. TestNG enables us to run tests without main().

--- Tes/src/test/java/Global/Website/Tests/AfterRegistrationTests/PlaylistTests.java
+++ Tes/src/test/java/Global/Website/Tests/AfterRegistrationTests/PlaylistTests.java
@@ -1,16 +1,22 @@
-package Java.Global.Website.Tests.LoginLogoutTests;
+package Global.Website.Tests.AfterRegistrationTests;
 
-@Test
-public void testGoogleSearch() {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class PlaylistTests {
+    @Test
+    public void testGoogleSearch() {
         // Optional, if not specified, WebDriver will search your path for chromedriver.
         System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
 
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com/xhtml");
-        Thread.sleep(5000);  // Let the user actually see something!
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("ChromeDriver");
         searchBox.submit();
-        Thread.sleep(5000);  // Let the user actually see something!
         driver.quit();
-        }
+    }
+}
---
+++ Tes/src/test/java/Global/Website/Tests/LoginLogoutTests/SignInAndSignOut.java
@@ -0,0 +1,17 @@
+package Global.Website.Tests.LoginLogoutTests;
+import org.testng.annotations.Test;
+
+
+public class SignInAndSignOut {
+    @Test
+    public void firstTestCase()
+    {
+        System.out.println("im in first test case from demoOne Class");
+    }
+
+    @Test
+    public void secondTestCase()
+    {
+        System.out.println("im in second test case from demoOne Class");
+    }
+}
