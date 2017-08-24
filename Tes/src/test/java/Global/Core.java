e6d04f25878c9d0008f87256cb5a025470f9b01b
Uy Nguyen <uy.nguyen@tidal.com>
2017-08-24 17:04:42 +0200

    Implemented core which is extended by website core. Added possibility for multiple chromedrivers.

--- Tes/src/test/java/Global/Core.java
+++ Tes/src/test/java/Global/Core.java
@@ -1,59 +1,47 @@
 package Global;
-
 import Website.WebsiteConstants;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import java.util.concurrent.TimeUnit;
-
-import static java.lang.Thread.sleep;
-
 
 public class Core {
     // Will always run first for tests that requires user to be signed in.
     // Future goal is also to make driver global, so that we can grab it from anywhere ;)
 
+    protected static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
+
+
     @BeforeMethod(alwaysRun = true)
-    public void basicWebclientSetup() {
-        //Set environment blabla, to be featured later
-        setupLogin("uytestautomation@gmail.com","testautomation");
+    public void globalSetup() {
+        for (int driverSetAttempts = 0; driverSetAttempts < 5; driverSetAttempts++) {
+            try {
+                threadDriver.set(setDriver());
+                break;
+            } catch (Exception ex) {
+                try {
+                    getDriver().quit();
+                } catch (NullPointerException nullPtrEx)
+                {
+                    //If it wasn't properly started, it can't be quitted
+                }
+                threadDriver.remove();
+            }
+        }
     }
 
-    // Will go to youtube.com, click log in manually, and proceed to log in.
-    public void setupLogin(String name, String pass) {
-            // Optional, if not specified, WebDriver will search your path for chromedriver.
-            WebDriver driver = new ChromeDriver();
-            driver.manage().timeouts().implicitlyWait(WebsiteConstants.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
-            driver.get("https://www.youtube.com");
+    public static WebDriver setDriver() {
+        WebDriver driver = null;
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(WebsiteConstants.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(WebsiteConstants.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
+        return driver;
+    }
 
-            // Currently using xPath because youtube has no consistent jss css that we can use for cssSelector.
-            WebElement elementToClick = driver.findElement(By.xpath("//*[contains(text(), 'Sign in')]"));
-            elementToClick.click();
+    public static WebDriver getDriver() {
 
-            // Input the email
-            WebElement inputFormEmail = driver.findElement(By.cssSelector("input[type='email']"));
-            inputFormEmail.sendKeys(name);
-            inputFormEmail.sendKeys(Keys.RETURN);
-
-
-        // Input the password, will implement webdriverwait in the future.
-        try {
-            sleep(1000);
-        } catch(InterruptedException e) {
-            System.out.println("gon wait");
-        }
-
-            WebElement inputFormPassword = driver.findElement(By.cssSelector("input[type='password']"));
-            inputFormPassword.sendKeys(pass);
-            inputFormPassword.sendKeys(Keys.RETURN);
-
-            // Attempts to find upload button, which is what should be on the screen when logged in.
-            // Might sometimes fail due to youtube changing to the new design..
-            driver.findElement(By.cssSelector("span.yt-uix-button-icon-material-upload"));
-
+        return threadDriver.get();
     }
 
     }
---
+++ Tes/src/test/java/Website/WebsiteCore.java
@@ -0,0 +1,64 @@
+package Website;
+
+import Global.Core;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
+
+public class WebsiteCore extends Core {
+    @BeforeMethod(alwaysRun = true)
+    public void basicWebclientSetup() {
+        //Set environment blabla, to be featured later
+        setupLogin("uytestautomation@gmail.com","testautomation");
+    }
+
+    // Will go to youtube.com, click log in manually, and proceed to log in.
+    public void setupLogin(String name, String pass) {
+        // Optional, if not specified, WebDriver will search your path for chromedriver.
+        WebDriver driver =  getDriver();
+        driver.manage().timeouts().implicitlyWait(WebsiteConstants.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
+        driver.get("https://www.youtube.com");
+
+        // Currently using xPath because youtube has no consistent jss css that we can use for cssSelector.
+        WebElement elementToClick = driver.findElement(By.xpath("//*[contains(text(), 'Sign in')]"));
+        elementToClick.click();
+
+        // Input the email
+        WebElement inputFormEmail = driver.findElement(By.cssSelector("input[type='email']"));
+        inputFormEmail.sendKeys(name);
+        inputFormEmail.sendKeys(Keys.RETURN);
+
+
+        // Input the password, will implement webdriverwait in the future.
+        try {
+            sleep(1000);
+        } catch(InterruptedException e) {
+            System.out.println("gon wait");
+        }
+
+
+        WebElement inputFormPassword = driver.findElement(By.cssSelector("input[type='password']"));
+        inputFormPassword.sendKeys(pass);
+        inputFormPassword.sendKeys(Keys.RETURN);
+
+        if(driver.findElements(By.cssSelector("div.vdE7Oc") ).size() != 0) { // If we have to do some nasty security check
+            driver.findElement(By.id("knowledgeLoginLocationInput")).sendKeys("Oslo");
+            driver.findElement(By.id("knowledgeLoginLocationInput")).sendKeys(Keys.RETURN);
+            driver.findElement(By.cssSelector("div.vdE7Oc")).click();
+
+        }
+
+        // Attempts to find upload button, which is what should be on the screen when logged in.
+        // Might sometimes fail due to youtube changing to the new design..
+        driver.findElement(By.cssSelector("yt-icon.style-scope"));
+
+    }
+
+}
